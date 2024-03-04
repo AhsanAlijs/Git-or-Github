66eba6fbf53991744223237ce571a541a3cd7be7
ahsanalijs <ahsanali45444@gmail.com>
2024-03-05 00:32:24 +0500

    add files

---
+++ app.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+
+
+if (body.style.backgroundColor === 'white') {
+    console.log('black');
+    body.style.backgroundColor = "black"
+} else {
+    body.style.backgroundColor = "white"
+}
